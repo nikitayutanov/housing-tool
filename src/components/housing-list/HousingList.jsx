3477f50110a9367e663b550fa55dcced259d46c5
Nikita Yutanov <nikitayutanov@gmail.com>
2021-07-29 11:15:42 +0500

    Add Housing List render

--- src/components/housing-list/HousingList.jsx
+++ src/components/housing-list/HousingList.jsx
@@ -4,18 +4,87 @@ import './HousingList.css';
 
 function HousingList({ companyId }) {
   const [housingStock, setHousingStock] = useState([]);
+  const [tree, setTree] = useState([]);
 
   useEffect(() => {
     const url = `${BASE_URL}/HousingStock?companyId=${companyId}`;
 
     fetch(url)
       .then((response) => response.json())
-      .then((data) => setHousingStock(data));
+      .then((data) => {
+        setHousingStock(data);
+
+        let currentStreetId = null;
+        let currentHouseId = null;
+        const streets = {};
+        const houses = {};
+
+        for (const item of data) {
+          // order of items in housingStock matters
+          const { streetId, streetName, houseId, building, corpus } = item;
+
+          if (streetId !== currentStreetId) {
+            currentStreetId = streetId;
+            streets[streetId] = {
+              id: streetId,
+              value: streetName,
+              children: [],
+            };
+          }
+          if (houseId !== currentHouseId) {
+            currentHouseId = houseId;
+            const address = corpus ? `${building}/${corpus}` : building;
+
+            houses[houseId] = {
+              parentId: streetId,
+              id: houseId,
+              value: address,
+              children: [],
+            };
+          }
+        }
+
+        for (const item of data) {
+          const { houseId } = item;
+          const newItem = { ...item, value: item.flat, id: item.addressId };
+          houses[houseId].children.push(newItem);
+        }
+
+        for (const house in houses) {
+          const { parentId } = houses[house];
+          streets[parentId].children.push(houses[house]);
+        }
+
+        const streetsArray = [];
+
+        for (const street in streets) {
+          streetsArray.push(streets[street]);
+        }
+
+        setTree(streetsArray);
+      });
   }, [companyId]);
 
-  const getList = () => {};
+  const handleClick = () => {};
 
-  return <div className="housing-list-wrapper">{getList()}</div>;
+  const renderTree = (tree) => {
+    return tree.map((node) => {
+      const { id, value, children } = node;
+
+      return (
+        <li key={id} onClick={handleClick}>
+          {value}
+          {children && <ul>{renderTree(children)}</ul>}
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div className="housing-list-wrapper">
+      {<ul className="housing-list">{renderTree(tree)}</ul>}
+    </div>
+  );
 }
 
 export default HousingList;
@@ -35,3 +104,40 @@ export default HousingList;
   </li>
 </ul>; */
 }
+
+// tree.map((street) => {
+//   const { id, value, children, isOpen } = street;
+
+//   return (
+//     <li key={id} data-id={id} onClick={handleClick}>
+//       {value}
+
+//       {children && (
+//         <ul className={isOpen ? 'houses' : 'houses hidden'}>
+//           {children.map((house) => {
+//             const { id, value, children } = house;
+//             // const address = corpus
+//             //   ? `${building}/${corpus}`
+//             //   : building;
+
+//             return (
+//               <li className="house" key={id} data-id={id}>
+//                 {value}
+
+//                 {children && (
+//                   <ul className="flats">
+//                     {children.map((flatItem) => {
+//                       const { value, id } = flatItem;
+
+//                       return <li key={id}>{value}</li>;
+//                     })}
+//                   </ul>
+//                 )}
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       )}
+//     </li>
+//   );
+// })
